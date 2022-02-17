9583401cfb3d2096cc97c28c526f5c90ba1d2c20
Alexey Dobriansky <dobriansky777@gmail.com>
2022-02-17 20:11:08 +0200

    solution 02

--- solution.go
+++ solution.go
@@ -5,13 +5,14 @@ import "math"
 type intCustomType int
 
 const (
-	SidesTriangle = intCustomType(3)
-	SidesSquare   = intCustomType(4)
-	SidesCircle   = intCustomType(0)
+	SidesTriangle intCustomType = 3
+	SidesSquare   intCustomType = 4
+	SidesCircle   intCustomType = 0
 )
 
 func CalcSquare(sideLen float64, sidesNum intCustomType) float64 {
 	var result float64 = 0
+
 	switch sidesNum {
 	case SidesTriangle:
 		result = math.Sqrt(3) * sideLen * sideLen / 4
@@ -20,5 +21,6 @@ func CalcSquare(sideLen float64, sidesNum intCustomType) float64 {
 	case SidesCircle:
 		result = math.Pi * sideLen * sideLen
 	}
+
 	return result
 }
